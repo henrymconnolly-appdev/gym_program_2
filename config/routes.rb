6749dc8f485c626ddcceffe8d7291af49951f57a
Raghu Betina <raghu@firstdraft.com>
2019-03-19 10:27:03 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "totals#index"
   # Routes for the Breakdown resource:
 
